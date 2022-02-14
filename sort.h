a033b55e9e5758b349bd7c3dff73d193b2c789c0
luiscolon0426 <luiscolon0426@gmail.com>
2022-02-14 12:57:20 +0000

    update header file

--- sort.h
+++ sort.h
@@ -31,6 +31,10 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+void swap(int *a, int *b);
+int part(int *array, int a, int b, size_t size);
+void qsorting(int *array, int a, int b, size_t size);
+
 
 
 
